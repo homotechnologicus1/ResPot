0b01f476931636a3a467be49a62db67adab874cd
joe <homo.technologicus1@gmail.com>
2020-12-06 20:31:56 +0900

    Initial Commit

---
+++ ResPot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResPot
+//
+//  Created by joe_mac on 12/06/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
