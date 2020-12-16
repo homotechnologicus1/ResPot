b1a1198e54da27762dfd65047d94d7d90fe2b0a4
joe <homo.technologicus1@gmail.com>
2020-12-16 23:50:57 +0900

    replace existing data arrays with a data model according to OOP concept

---
+++ ResPot/Model/Restaurant.swift
@@ -0,0 +1,29 @@
+//
+//  Restaurant.swift
+//  ResPot
+//
+//  Created by joe_mac on 12/16/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isVisited: Bool
+    
+    init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isVisited = isVisited
+    }
+    
+    convenience init() {
+        self.init(name: "", type: "", location: "", image: "", isVisited: false)
+    }
+}
