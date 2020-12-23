50b46fdef7bb82bc999541b0bff2d0ecdccfb4f4
joe <homo.technologicus1@gmail.com>
2020-12-24 02:50:05 +0900

    add MapKit feature

--- ResPot/Controller/RestaurantDetailViewController.swift
+++ ResPot/Controller/RestaurantDetailViewController.swift
@@ -67,7 +67,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -92,8 +92,27 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             
             return cell
             
+        case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailSeparatorCell.self), for: indexPath) as! RestaurantDetailSeparatorCell
+            cell.titleLabel.text = "HOW TO GET HERE"
+            
+            return cell
+            
+        case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self), for: indexPath) as! RestaurantDetailMapCell
+            cell.configure(location: restaurant.location)
+            
+            return cell
+            
         default:
             fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMap" {
+            let destinationController = segue.destination as! MapViewController
+            destinationController.restaurant = restaurant
         }
     }
 
---
+++ ResPot/View/RestaurantDetailSeparatorCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailSeparatorCell.swift
+//  ResPot
+//
+//  Created by joe_mac on 12/23/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailSeparatorCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
