1adcad1f12f4cf65e724ef10182d653696c08ffe
joe <homo.technologicus1@gmail.com>
2021-01-17 20:29:55 +0900

    add CloudKit framework and use iCloud service on Discover tab and NewRestaurantController

---
+++ ResPot/Controller/DiscoverTableViewController.swift
@@ -0,0 +1,196 @@
+//
+//  DiscoverTableViewController.swift
+//  ResPot
+//
+//  Created by joe_mac on 01/11/2021.
+//  Copyright © 2021 Joe K. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class DiscoverTableViewController: UITableViewController {
+    
+    var restaurants: [CKRecord] = []
+    
+    var spinner = UIActivityIndicatorView()
+    
+    private var imageCache = NSCache<CKRecord.ID, NSURL>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        }
+        
+        // Show the activity indicator
+        spinner.style = UIActivityIndicatorView.Style.medium
+        spinner.hidesWhenStopped = true
+        view.addSubview(spinner)
+        
+        // Define layout constraints for the spinner
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150.0),
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+        
+        // Activate the spinner
+        spinner.startAnimating()
+        
+        // Fetch record from the public database
+        fetchRecordsFromCloud()
+        
+        // Pull To Refresh Control
+        refreshControl = UIRefreshControl()
+        refreshControl?.backgroundColor = UIColor.white
+        refreshControl?.tintColor = UIColor.gray
+        refreshControl?.addTarget(self, action: #selector(fetchRecordsFromCloud), for: UIControl.Event.valueChanged)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurants.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
+        
+        // Configure the cell...
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        
+//        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
+//            if let imageData = try? Data(contentsOf: imageAsset.fileURL!) {
+//                cell.imageView?.image = UIImage(data: imageData)
+//            }
+//        }
+        
+        cell.imageView?.image = UIImage(named: "photo")
+        
+        // Check if the image is stored in cache
+        if let imageFileURL = imageCache.object(forKey: restaurant.recordID) {
+            // Fetch image from cache
+            print("Get image from cache")
+            if let imageData = try? Data.init(contentsOf: imageFileURL as URL) {
+                cell.imageView?.image = UIImage(data: imageData)
+            }
+            
+        } else {
+            // Fetch Image from Cloud in background
+            let publicDatabase = CKContainer.default().publicCloudDatabase
+            let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs: [restaurant.recordID])
+            fetchRecordsImageOperation.desiredKeys = ["image"]
+            fetchRecordsImageOperation.queuePriority = .veryHigh
+            
+            fetchRecordsImageOperation.perRecordCompletionBlock = { (record, recordID, error) -> Void in
+                if let error = error {
+                    print("Failed to get restaurant image: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let restaurantRecord = record,
+                   let image = restaurantRecord.object(forKey: "image"),
+                   let imageAsset = image as? CKAsset {
+                    
+                    if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) {
+                        
+                        // Replace the placeholder image with the restaurant image
+                        DispatchQueue.main.async {
+                            cell.imageView?.image = UIImage(data: imageData)
+                            cell.setNeedsLayout()
+                        }
+                        
+                        // Add the image URL to cache
+                        self.imageCache.setObject(imageAsset.fileURL! as NSURL, forKey: restaurant.recordID)
+                    }
+                }
+            }
+            publicDatabase.add(fetchRecordsImageOperation)
+        }
+        
+        
+        
+        return cell
+    }
+
+    // MARK: - Helper methods
+    
+    @objc func fetchRecordsFromCloud() {
+        
+        // Remove existing records before refreshing
+        restaurants.removeAll()
+        tableView.reloadData()
+        
+        // Fetch data using Convenience API
+        let cloudContainer = CKContainer.default()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        
+        /*  using Convenience API
+        publicDatabase.perform(query, inZoneWith: nil, completionHandler: { (results, error) -> Void in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let results = results {
+                print("Completed the download of Restaurant data")
+                self.restaurants = results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        })  */
+        
+        query.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        // Create the query operation with the query
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.desiredKeys = ["name"]
+        queryOperation.queuePriority = .veryHigh
+        queryOperation.resultsLimit = 50
+        queryOperation.recordFetchedBlock = { (record) -> Void in
+            self.restaurants.append(record)
+        }
+        
+        queryOperation.queryCompletionBlock = { (cursor, error) -> Void in
+            if let error = error {
+                print("Failed to get data from iCloud - \(error.localizedDescription)")
+                return
+            }
+            
+            print("Successfully retrieve the data from iCloud")
+            DispatchQueue.main.async {
+                self.spinner.stopAnimating()
+                self.tableView.reloadData()
+                
+                if let refreshControl = self.refreshControl {
+                    if refreshControl.isRefreshing {
+                        refreshControl.endRefreshing()
+                    }
+                }
+            }
+        }
+        
+        // Execute the query
+        publicDatabase.add(queryOperation)
+        
+    }
+
+}
